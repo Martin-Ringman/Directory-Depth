0d35a0302dfdd079b2eb1e447babaa46edfb319b
Martin-Ringman <a.ringman@gmail.com>
2022-10-29 20:22:13 -0700

    save

---
+++ dird.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <dirent.h>
+
+
+//opendir(2)
+//readdir(2)
+//closedir(2)
+
+//getopt(3)
+//asprintf(3)
+
+
+void search(char* dir){
+//int maxDepth = 0;
+
+    //opendir
+    //loop
+        //readdir
+        //if entry is a directory
+            //int depth = search()
+            //if depth > maxDepth
+                //maxDepth = depth
+    //closedir   
+
+    //return maxDepth + 1;
+}
+
+
+int main(int argc, char** argv) {
+    
+    
+
+             
+}
